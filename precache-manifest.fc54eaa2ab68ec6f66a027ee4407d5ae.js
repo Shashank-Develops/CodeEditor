c537127d3b1e9296462e10a7cab46509bff3f1eb
Shashankdhauni <112565970+Shashankdhauni@users.noreply.github.com>
2023-11-24 20:03:22 +0530

    Updates

---
+++ precache-manifest.fc54eaa2ab68ec6f66a027ee4407d5ae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10d4af4eb1a96cdc9ba6f544f4a73a43",
+    "url": "/CodeEditor/index.html"
+  },
+  {
+    "revision": "07ba295eb3a48be15cfe",
+    "url": "/CodeEditor/static/css/2.ba2e9970.chunk.css"
+  },
+  {
+    "revision": "9cc9169cf051437b38ce",
+    "url": "/CodeEditor/static/css/main.09733d98.chunk.css"
+  },
+  {
+    "revision": "07ba295eb3a48be15cfe",
+    "url": "/CodeEditor/static/js/2.5232e13d.chunk.js"
+  },
+  {
+    "revision": "38d5cc1cb1001b9cc38b06e2da080c18",
+    "url": "/CodeEditor/static/js/2.5232e13d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9cc9169cf051437b38ce",
+    "url": "/CodeEditor/static/js/main.9bb20675.chunk.js"
+  },
+  {
+    "revision": "b7f512ce5c71416ef15a",
+    "url": "/CodeEditor/static/js/runtime-main.96ffbaa4.js"
+  }
+]);
